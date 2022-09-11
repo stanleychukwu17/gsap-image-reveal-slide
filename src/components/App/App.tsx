f32bc367dcd8ddf98acf55ee91b90aaa738ea47e
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-11 19:55:15 +0100

    modified the color of the check svg icon brought in the location and check svg to the home page

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,11 @@
-import './app.scss';
 import { motion } from 'framer-motion';
 
+import './app.scss';
+
+// importing of assets
 import mule from '../../assets/images/mule.png'
+import check from '../../assets/svg/circle-check.svg'
+import location from '../../assets/svg/location.svg'
 
 
 
@@ -12,14 +16,17 @@ const App = () => {
             <div className="TopGuy">                
                 <div className="TopGuy__header">
                     <div className="">StickerMule</div>
-                    <div className="">Home of stickers</div>
+                    <div className="">
+                        <div className=""><img src={check} alt="" /></div>
+                        <div className="">Home of stickers</div>
+                    </div>
                 </div>
                 <div className="TopGuy__midImg">
                     <img src={mule} alt="" />
                 </div>
                 <div className="TopGuy__lastMen">
                     <div className="">
-                        <div className=""></div>
+                        <div className=""><img src={location} alt="" /></div>
                         <div className="">Amsterdam, New York</div>
                     </div>
                     <div className="">
