111635cf64cab6600b82b640ab792c6669d66f4d
stanley chukwu <stanleychukwu17@yahoo.com>
2022-09-14 15:42:31 +0100

    done with all the design

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -8,6 +8,7 @@ import './app.scss';
 
 // importing of assets
 import mule from '../../assets/images/mule.png'
+import mule2 from '../../assets/images/mule2.jpg'
 import check from '../../assets/svg/circle-check.svg'
 import location from '../../assets/svg/location.svg'
 
@@ -49,16 +50,28 @@ const App = () => {
                         Nulla non proident aliqua deserunt magna pariatur proident veniam deserunt proident.
                     </div>
                 </div>
-                <div className="">
-                    <div className="">
-                        <div className=""></div>
-                        <div className="">Ex irure sunt consectetur velit deserunt consectetur mollit sunt ipsum minim aliquip. Non amet elit in veniam cupidatat sint commodo qui. In sunt aliquip culpa magna. Mollit incididunt ex id dolore et pariatur cillum proident culpa elit sit. Proident officia occaecat irure dolore consequat culpa tempor ea laboris nulla est exercitation.</div>
+                <div className="BtmMidMain">
+                    <div className="BtmMid_1">
+                        <div className=""><img src={mule2} alt="" /></div>
+                        <div className="">
+                            Ex irure sunt consectetur velit deserunt consectetur mollit sunt ipsum minim
+                            aliquip. Non amet elit in veniam cupidatat sint commodo qui. In sunt aliquip culpa magna.
+                        </div>
                     </div>
-                    <div className="">
-                        Deserunt dolore nisi consequat laborum fugiat deserunt duis culpa incididunt. Exercitation cupidatat in in laborum voluptate aliquip id nisi non cillum excepteur deserunt nostrud. Dolore ex nulla consectetur nulla do ad ut ad non laboris anim id aute laboris. Anim cillum deserunt non ea do deserunt. Consequat proident cillum ad labore adipisicing nulla exercitation aliquip eu ullamco do non voluptate. Consectetur ullamco eu culpa id quis commodo nostrud quis culpa ea ad.
+                    <div className="BtmMid_2">
+                        Deserunt dolore nisi consequat laborum fugiat deserunt duis culpa incididunt. Exercitation
+                        cupidatat in in laborum voluptate aliquip id nisi non cillum excepteur deserunt nostrud. Dolore
+                        ex nulla consectetur nulla do ad ut ad non laboris anim id aute laboris. Anim cillum deserunt non
+                        ea do deserunt. Consequat proident cillum ad labore adipisicing nulla exercitation aliquip eu
+                        ullamco do non voluptate. Consectetur ullamco eu culpa id quis commodo nostrud quis culpa ea ad
+                        ullamco do non voluptate. Consectetur ullamco eu culpa id quis commodo nostrud quis culpa ea ad
+                        ea do deserunt. Consequat proident cillum ad labore adipisicing nulla exercitation aliquip eu
+                        ullamco do non voluptate. Consectetur ullamco eu culpa id quis commodo nostrud quis culpa ea ad
+                        ullamco do non voluptate. Consectetur ullamco eu culpa id quis commodo nostrud quis culpa ea ad
+                        ea do deserunt. Consequat proident cillum ad labore adipisicing nulla exercitation aliquip eu
                     </div>
                 </div>
-                <div className="">
+                <div className="BtmLastM">
                     <div className="">Designed By STANLEY</div>
                     <div className="">2022</div>
                 </div>
